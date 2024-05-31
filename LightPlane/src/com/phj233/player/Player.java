c458da48967ee2f82c3acfcec9c19dca9a0886b5
phj233 <2780990934@qq.com>
2024-05-31 21:12:58 +0800

    添加player血量

--- LightPlane/src/com/phj233/player/Player.java
+++ LightPlane/src/com/phj233/player/Player.java
@@ -1,10 +1,15 @@
-package com.phj233;
+package com.phj233.player;
+import com.phj233.common.BaseFrame;
+import com.phj233.common.MyPlane;
+import com.phj233.item.Item;
+
 import java.awt.*;
 
 public class Player {
     public MyPlane myPlane;
-    public  int width=102;
-    public  int height =126;
+    public int hp=5;
+    public int width=102;
+    public int height =126;
     public int x,y;
     public int attackMode=0;
     public Image[] images=new Image[]{
@@ -12,23 +17,31 @@ public class Player {
             Toolkit.getDefaultToolkit().getImage("images/player2.png")
 
     };
-    public int imageindex=0;
+    public int imageIndex =0;
     public Player(MyPlane myPlane){
         this.myPlane=myPlane;
         this.x=(BaseFrame.frameWidth-this.width)/2;
         this.y=BaseFrame.frameHeight-this.height *2;
     }
     public void drawSelf(Graphics g){
-        g.drawImage(this.images[imageindex],x,y,width, height,null );
+        g.drawImage(this.images[imageIndex],x,y,width, height,null );
         if (this.myPlane.timer%50==0){
-            imageindex++;
-            if (this.imageindex==this.images.length) this.imageindex=0;
+            imageIndex++;
+            if (this.imageIndex ==this.images.length) this.imageIndex =0;
         }
         for(int i=0;i<this.myPlane.items.size();i++) {
             Item item=this.myPlane.items.get(i);
             if((this.x>=item.x-this.width && this.x<=item.x+item.width) && (this.y>=item.y-this.height && this.y<=item.y+item.height) ) {
                 item.eated();
             }
+        }
+    }
+
+    //减少血量
+    public void reduceHp() {
+        this.hp--;
+        if(this.hp<=0) {
+            this.myPlane.gameOver();
         }
     }
 
